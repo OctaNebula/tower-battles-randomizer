b09b04faf0f3fce75c452d36f6ce68b07364f572
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-12-23 17:24:13 +0100

    fixed attributeError on window

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ def randomize():
     #picks a random picture from the assets/towers/earlygame folder and sets it as the texture for the slot1 entity
     slot1.alpha = 1
     for i in range(random.randint(1, 50)):
-        slot1.texture = random.choice(os.listdir(os.getcwd() + 'assets/towers/earlygame'))
+        slot1.texture = random.choice(os.listdir('assets\\towers\\earlygame\\'))
         time.sleep(0.01)
     slot2.alpha = 1
     for i in range(random.randint(1, 50)):
-        slot2.texture = random.choice(os.listdir(os.getcwd() + 'assets/towers/midgame'))
+        slot2.texture = random.choice(os.listdir('assets\\towers\\midgame'))
         time.sleep(0.01)
     slot3.alpha = 1
     for i in range(random.randint(1, 50)):
-        slot3.texture = random.choice(os.listdir(os.getcwd() + 'assets/towers/midgame'))
+        slot3.texture = random.choice(os.listdir('assets\\towers\\midgame'))
         time.sleep(0.01)
     slot4.alpha = 1
     for i in range(random.randint(1, 50)):
-        slot4.texture = random.choice(os.listdir(os.getcwd() + 'assets/towers/lategame'))
+        slot4.texture = random.choice(os.listdir('assets\\towers\\lategame'))
         time.sleep(0.01)
     slot5.alpha = 1
     for i in range(random.randint(1, 50)):
-        slot5.texture = random.choice(os.listdir(os.getcwd() + 'assets/towers/support'))
+        slot5.texture = random.choice(os.listdir('assets\\towers\\support'))
         time.sleep(0.01)
 
 def randomizeFullRandom():
@@ -74,14 +74,14 @@ window.borderless = False
 window.fullscreen = False
 window.exit_button.visible = False
 window.fps_counter.enabled = False
-window.entity_counter.enabled = False
-window.collider_counter.enabled = False
+#window.entity_counter.enabled = False
+#window.collider_counter.enabled = False
 window.color = color.rgb(255, 255, 255)
 window.size = (800, 600)
 window.title = 'Tower Battles tower randomizer'
 window.cog_button.visible = False
 
-mainScreen = Entity(model='quad', texture='assets/menus/mainRandomizerScreen', scale=(16, 12), z=1)
+mainScreen = Entity(model='quad', texture='assets\\menus\\mainRandomizerScreen', scale=(16, 12), z=1)
 
 
 randomizeButton = Button(model="quad", scale=(0.31, 0.125), position=(0.03, -0.19), z=2, alpha=0)
@@ -100,3 +100,4 @@ app.run()
 
 
 
+input()
